225283327a8bbd7717469ffe112bbaddca356f02
Koncopd <koncopd@gmail.com>
2020-09-24 21:01:08 +0200

    add max_genes filter

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # add binary I of size n_vars x number of annotated terms in files
 # if I[i,j]=1 then gene i is active in annotation j
-def add_annotations(adata, files, min_genes=0, varm_key='I', uns_key='terms'):
+def add_annotations(adata, files, min_genes=0, max_genes=None, varm_key='I', uns_key='terms'):
     files = [files] if isinstance(files, str) else files
     annot = []
 
@@ -16,7 +16,10 @@ def add_annotations(adata, files, min_genes=0, varm_key='I', uns_key='terms'):
     I = [[int(gene in term) for term in annot] for gene in var_names]
     I = np.asarray(I, dtype='int32')
 
+
     mask = I.sum(0) > min_genes
+    if max_genes is not None:
+        mask &= I.sum(0) < max_genes
     I = I[:, mask]
     adata.varm[varm_key] = I
     adata.uns[uns_key] = [term[0] for i, term in enumerate(annot) if i not in np.where(~mask)[0]]
